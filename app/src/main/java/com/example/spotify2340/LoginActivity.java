0b498692d5f31c3d5f0c9d33c039cfd4a6e35b65
Bratee Podder <bpodder3@gatech.edu>
2024-03-19 20:46:02 -0400

    Still working on login: + Made UI more proper (still working on it) + Added premature code for diverting to main page instead of showing login page if user is already logged in

--- app/src/main/java/com/example/spotify2340/LoginActivity.java
+++ app/src/main/java/com/example/spotify2340/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.spotify2340;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.spotify2340.ui.home.AccountFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    // Code for starting AccountFragment (later replace) instead of login
+//    @Override
+//    protected void onStart() {
+//        if (email != null && password != null) {
+//            Intent i = new Intent(LoginActivity.this, AccountFragment.class);
+//            startActivity(i);
+//        }
+//    }
 }
